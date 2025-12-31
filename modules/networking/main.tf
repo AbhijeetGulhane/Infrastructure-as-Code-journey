9d2f45a42b81cccf73a17536fcc316bbbc5fb834
Abhijeet Gulhane <abhijeet.gulhane.317@gmail.com>
2025-12-30 22:18:13 -0500

    Changed gitignore

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -10,17 +10,17 @@ resource "aws_security_group" "nat_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     # FIX: Changed to plural 'cidr_blocks' and added brackets []
-    cidr_blocks = [var.vpc_cidr] 
+    cidr_blocks = [var.vpc_cidr]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     # FIX: Changed to plural 'cidr_blocks' and added brackets []
     cidr_blocks = ["0.0.0.0/0"]
   }
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,5 +1,5 @@
 variable "project_name" { type = string }
-variable "vpc_cidr"     { type = string }
+variable "vpc_cidr" { type = string }
 
 variable "public_subnets" {
   type = map(object({ cidr = string, az = string }))
